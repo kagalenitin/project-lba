47f34245031e651c43d0bfe74e06f3a2485e0261
payal.patel99@gmail.com <payal.patel99@gmail.com@ad611e3e-4fe3-d6cf-8c8c-150ac5cd8cda>
2010-09-02 19:19:43 +0000

    initial commit

--- trunk/LocationBasedAd/src/com/lba/login/Channel.java
+++ trunk/LocationBasedAd/src/com/lba/login/Channel.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
