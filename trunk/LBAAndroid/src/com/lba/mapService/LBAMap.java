ec9d2a135622ea93b9b5c0439ca9dbbdccef0a86
payal.patel99@gmail.com <payal.patel99@gmail.com@ad611e3e-4fe3-d6cf-8c8c-150ac5cd8cda>
2010-11-02 06:23:15 +0000

    updated with current location on single map and direction. committing for the next level

--- trunk/LBAAndroid/src/com/lba/mapService/LBAMap.java
+++ trunk/LBAAndroid/src/com/lba/mapService/LBAMap.java
@@ -1,22 +1,35 @@
 package com.lba.mapService;
 
 import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.restlet.ext.xml.DomRepresentation;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.RectF;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -32,6 +45,7 @@ import com.google.android.maps.MapController;
 import com.google.android.maps.MapView;
 import com.google.android.maps.MapView.LayoutParams;
 import com.google.android.maps.Overlay;
+import com.google.android.maps.Projection;
 import com.lba.R;
 import com.lba.beans.AdMerchantAdBean;
 import com.lba.home.WelcomeUser;
@@ -55,8 +69,6 @@ public class LBAMap extends MapActivity implements LocationListener {
 	private String adName = "";
 	private String longitude = "";
 	private String latitude = "";
-	// ArrayList latitudes = new ArrayList();
-	// ArrayList longitudes = new ArrayList();
 
 	ArrayList<AdMerchantAdBean> advertisements = new ArrayList<AdMerchantAdBean>();
 
@@ -134,7 +146,7 @@ public class LBAMap extends MapActivity implements LocationListener {
 			p = new GeoPoint((int) (lat * 1E6), (int) (lng * 1E6));
 			points.add(p);
 			mc.animateTo(p);
-			mc.setZoom(10);
+			// mc.setZoom(13);
 		}
 		if (advertisements.size() == 0) {
 			Toast.makeText(getBaseContext(),
@@ -148,7 +160,39 @@ public class LBAMap extends MapActivity implements LocationListener {
 		// listOfOverlays.clear();
 		listOfOverlays.add(mapOverlay);
 		mapView.invalidate();
+		mapView.setBuiltInZoomControls(true);
+		lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
+		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		LocationListener myLocationListener = new MyLocationListener();
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+				1000L, 500.0f, myLocationListener);
+		Location currentLocation = locationManager
+				.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		try {
+			double Latitude = currentLocation.getLatitude();
+			double Longitude = currentLocation.getLongitude();
+			// adds the user location as a point on the map
+			p = new GeoPoint((int) (Latitude * 1e6), (int) (Longitude * 1e6));
+			mc.animateTo(p);
+		} catch (Exception e) {
+			Toast.makeText(getBaseContext(),
+					"No current locaiton information available!",
+					Toast.LENGTH_SHORT).show();
+		}
+
+		double src_lat = 37.3492097; // the testing source
+		double src_long = -122.0326019;
+		double dest_lat = 37.33065; // the testing destination
+		double dest_long = -121.905105;
+		GeoPoint srcGeoPoint = new GeoPoint((int) (src_lat * 1E6),
+				(int) (src_long * 1E6));
+		GeoPoint destGeoPoint = new GeoPoint((int) (dest_lat * 1E6),
+				(int) (dest_long * 1E6));
+
+		DrawPath(srcGeoPoint, destGeoPoint, Color.GREEN, mapView);
+
+		mapView.getController().animateTo(srcGeoPoint);
 	}
 
 	@Override
@@ -178,11 +222,27 @@ public class LBAMap extends MapActivity implements LocationListener {
 				Bitmap bmp = BitmapFactory.decodeResource(getResources(),
 						R.drawable.blackblank);
 
-				canvas.drawBitmap(bmp, screenPts.x, screenPts.y - 50, null);
+				canvas.drawBitmap(bmp, screenPts.x - 6, screenPts.y - 6, null);
 				// canvas.drawText(advertisements.get(i).getAdName(),
 				// screenPts.x,
 				// screenPts.y - 50, paint);
 			}
+
+			// ---translate the GeoPoint to screen pixels---
+			Point screenPts = new Point();
+			Paint paint = new Paint();
+			paint.setStrokeWidth(1);
+			paint.setARGB(255, 140, 23, 23);
+			// paint.setStyle(Paint.Style.STROKE);
+			paint.setTextSize(15);
+
+			mapView.getProjection().toPixels(p, screenPts);
+
+			// ---add the marker---
+			Bitmap bmp = BitmapFactory.decodeResource(getResources(),
+					R.drawable.redblank);
+
+			canvas.drawBitmap(bmp, screenPts.x - 6, screenPts.y - 6, null);
 
 			return true;
 		}
@@ -193,10 +253,10 @@ public class LBAMap extends MapActivity implements LocationListener {
 			if (event.getAction() == 1) {
 				GeoPoint p = mapView.getProjection().fromPixels(
 						(int) event.getX(), (int) event.getY());
-				Toast.makeText(
-						getBaseContext(),
-						p.getLatitudeE6() / 1E6 + "," + p.getLongitudeE6()
-								/ 1E6, Toast.LENGTH_SHORT).show();
+				// Toast.makeText(
+				// getBaseContext(),
+				// p.getLatitudeE6() / 1E6 + "," + p.getLongitudeE6()
+				// / 1E6, Toast.LENGTH_SHORT).show();
 			}
 
 			if (advertisements.size() == 0) {
@@ -211,31 +271,20 @@ public class LBAMap extends MapActivity implements LocationListener {
 
 	@Override
 	public void onLocationChanged(Location location) {
+		System.out.println(Log.VERBOSE + "LOCATION CHANGED");
 		if (location != null) {
 			double lat = location.getLatitude();
 			double lng = location.getLongitude();
-			GeoPoint p = new GeoPoint((int) lat * 1000000, (int) lng * 1000000);
+			p = new GeoPoint((int) (lat * 1000000), (int) (lng * 1000000));
 			mc.animateTo(p);
 			// ---Add a location marker---
 			MapOverlay mapOverlay = new MapOverlay();
 			List<Overlay> listOfOverlays = mapView.getOverlays();
 			// listOfOverlays.clear();
 			listOfOverlays.add(mapOverlay);
-			mapView.invalidate();
+			// mapView.invalidate();
 		}
 	}
-
-	/* User can zoom in/out using keys provided on keypad */
-	/*
-	 * public boolean onKeyDown(int keyCode, KeyEvent event) { if (keyCode ==
-	 * KeyEvent.KEYCODE_I) {
-	 * mapView.getController().setZoom(mapView.getZoomLevel() + 1); return true;
-	 * } else if (keyCode == KeyEvent.KEYCODE_O) {
-	 * mapView.getController().setZoom(mapView.getZoomLevel() - 1); return true;
-	 * } else if (keyCode == KeyEvent.KEYCODE_S) { mapView.setSatellite(true);
-	 * return true; } else if (keyCode == KeyEvent.KEYCODE_T) {
-	 * mapView.setTraffic(true); return true; } return false; }
-	 */
 
 	@Override
 	public void onProviderDisabled(String provider) {
@@ -249,7 +298,16 @@ public class LBAMap extends MapActivity implements LocationListener {
 
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
+		if (status == LocationProvider.AVAILABLE) {
 
+		}
+		if (status == LocationProvider.OUT_OF_SERVICE) {
+			// ViewAdapter.showShortToast(mContext,
+			// "Location is temporarily out of service.");
+		}
+		if (status == LocationProvider.TEMPORARILY_UNAVAILABLE) {
+
+		}
 	}
 
 	@Override
@@ -280,5 +338,243 @@ public class LBAMap extends MapActivity implements LocationListener {
 			break;
 		}
 		return true;
+	}
+
+	protected class MyLocationOverlay extends com.google.android.maps.Overlay {
+
+		@Override
+		public boolean draw(Canvas canvas, MapView mapView, boolean shadow,
+				long when) {
+			Paint paint = new Paint();
+
+			super.draw(canvas, mapView, shadow);
+			// Converts lat/lng-Point to OUR coordinates on the screen.
+			Point myScreenCoords = new Point();
+
+			mapView.getProjection().toPixels(p, myScreenCoords);
+
+			paint.setStrokeWidth(1);
+			paint.setARGB(255, 255, 255, 255);
+			paint.setStyle(Paint.Style.STROKE);
+
+			Bitmap bmp = BitmapFactory.decodeResource(getResources(),
+					R.drawable.blackblank);
+
+			canvas.drawBitmap(bmp, myScreenCoords.x, myScreenCoords.y, paint);
+			canvas.drawText("I am here...", myScreenCoords.x, myScreenCoords.y,
+					paint);
+			return true;
+		}
+	}
+
+	private class MyLocationListener implements LocationListener {
+
+		public void onLocationChanged(Location argLocation) {
+			GeoPoint myGeoPoint = new GeoPoint(
+					(int) (argLocation.getLatitude() * 1000000),
+					(int) (argLocation.getLongitude() * 1000000));
+			/*
+			 * it will show a message on location change
+			 * Toast.makeText(getBaseContext(), "New location latitude ["
+			 * +argLocation.getLatitude() + "] longitude [" +
+			 * argLocation.getLongitude()+"]", Toast.LENGTH_SHORT).show();
+			 */
+
+			mc.animateTo(myGeoPoint);
+
+		}
+
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+		}
+
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+		}
+	}
+
+	// ============================================================//
+
+	public class MyOverLay extends Overlay {
+		private GeoPoint gp1;
+		private GeoPoint gp2;
+		private int mRadius = 6;
+		private int mode = 0;
+		private int defaultColor;
+		private String text = "";
+		private Bitmap img = null;
+
+		public MyOverLay(GeoPoint gp1, GeoPoint gp2, int mode) // GeoPoint is a
+		// int. (6E)
+		{
+			this.gp1 = gp1;
+			this.gp2 = gp2;
+			this.mode = mode;
+			defaultColor = 999; // no defaultColor
+
+		}
+
+		public MyOverLay(GeoPoint gp1, GeoPoint gp2, int mode, int defaultColor) {
+			this.gp1 = gp1;
+			this.gp2 = gp2;
+			this.mode = mode;
+			this.defaultColor = defaultColor;
+		}
+
+		public void setText(String t) {
+			this.text = t;
+		}
+
+		public void setBitmap(Bitmap bitmap) {
+			this.img = bitmap;
+		}
+
+		public int getMode() {
+			return mode;
+		}
+
+		@Override
+		public boolean draw(Canvas canvas, MapView mapView, boolean shadow,
+				long when) {
+			Projection projection = mapView.getProjection();
+			if (shadow == false) {
+				Paint paint = new Paint();
+				paint.setAntiAlias(true);
+				Point point = new Point();
+				projection.toPixels(gp1, point);
+				// mode=1&#65306;start
+				if (mode == 1) {
+					if (defaultColor == 999)
+						paint.setColor(Color.BLUE);
+					else
+						paint.setColor(defaultColor);
+					RectF oval = new RectF(point.x - mRadius,
+							point.y - mRadius, point.x + mRadius, point.y
+									+ mRadius);
+					// start point
+					canvas.drawOval(oval, paint);
+				}
+				// mode=2&#65306;path
+				else if (mode == 2) {
+					if (defaultColor == 999)
+						paint.setColor(Color.RED);
+					else
+						paint.setColor(defaultColor);
+					Point point2 = new Point();
+					projection.toPixels(gp2, point2);
+					paint.setStrokeWidth(5);
+					paint.setAlpha(120);
+					canvas.drawLine(point.x, point.y, point2.x, point2.y, paint);
+				}
+				/* mode=3&#65306;end */
+				else if (mode == 3) {
+					/* the last path */
+
+					if (defaultColor == 999)
+						paint.setColor(Color.GREEN);
+					else
+						paint.setColor(defaultColor);
+					Point point2 = new Point();
+					projection.toPixels(gp2, point2);
+					paint.setStrokeWidth(5);
+					paint.setAlpha(120);
+					canvas.drawLine(point.x, point.y, point2.x, point2.y, paint);
+					RectF oval = new RectF(point2.x - mRadius, point2.y
+							- mRadius, point2.x + mRadius, point2.y + mRadius);
+					/* end point */
+					paint.setAlpha(255);
+					canvas.drawOval(oval, paint);
+				}
+			}
+			return super.draw(canvas, mapView, shadow, when);
+		}
+
+	}
+
+	private void DrawPath(GeoPoint src, GeoPoint dest, int color,
+			MapView mMapView01) {
+		// connect to map web service
+		StringBuilder urlString = new StringBuilder();
+		urlString.append("http://maps.google.com/maps?f=d&hl=en");
+		urlString.append("&saddr=");// from
+		urlString.append(Double.toString((double) src.getLatitudeE6() / 1.0E6));
+		urlString.append(",");
+		urlString
+				.append(Double.toString((double) src.getLongitudeE6() / 1.0E6));
+		urlString.append("&daddr=");// to
+		urlString
+				.append(Double.toString((double) dest.getLatitudeE6() / 1.0E6));
+		urlString.append(",");
+		urlString
+				.append(Double.toString((double) dest.getLongitudeE6() / 1.0E6));
+		urlString.append("&ie=UTF8&0&om=0&output=kml");
+		Log.d("xxx", "URL=" + urlString.toString());
+		// get the kml (XML) doc. And parse it to get the coordinates(direction
+		// route).
+		Document doc = null;
+		HttpURLConnection urlConnection = null;
+		URL url = null;
+		try {
+			url = new URL(urlString.toString());
+			urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setRequestMethod("GET");
+			urlConnection.setDoOutput(true);
+			urlConnection.setDoInput(true);
+			urlConnection.connect();
+
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			doc = db.parse(urlConnection.getInputStream());
+
+			if (doc.getElementsByTagName("GeometryCollection").getLength() > 0) {
+				// String path =
+				// doc.getElementsByTagName("GeometryCollection").item(0).getFirstChild().getFirstChild().getNodeName();
+				String path = doc.getElementsByTagName("GeometryCollection")
+						.item(0).getFirstChild().getFirstChild()
+						.getFirstChild().getNodeValue();
+				Log.d("xxx", "path=" + path);
+				String[] pairs = path.split(" ");
+				String[] lngLat = pairs[0].split(","); // lngLat[0]=longitude
+				// lngLat[1]=latitude
+				// lngLat[2]=height
+				// src
+				GeoPoint startGP = new GeoPoint(
+						(int) (Double.parseDouble(lngLat[1]) * 1E6),
+						(int) (Double.parseDouble(lngLat[0]) * 1E6));
+				mMapView01.getOverlays()
+						.add(new MyOverLay(startGP, startGP, 1));
+				GeoPoint gp1;
+				GeoPoint gp2 = startGP;
+				for (int i = 1; i < pairs.length; i++) // the last one would be
+				// crash
+				{
+					lngLat = pairs[i].split(",");
+					gp1 = gp2;
+					// watch out! For GeoPoint, first:latitude, second:longitude
+					gp2 = new GeoPoint(
+							(int) (Double.parseDouble(lngLat[1]) * 1E6),
+							(int) (Double.parseDouble(lngLat[0]) * 1E6));
+					mMapView01.getOverlays().add(
+							new MyOverLay(gp1, gp2, 2, color));
+					Log.d("xxx", "pair:" + pairs[i]);
+				}
+				mMapView01.getOverlays().add(new MyOverLay(dest, dest, 3)); // use
+				// the
+				// default
+				// color
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (SAXException e) {
+			e.printStackTrace();
+		}
 	}
 }
